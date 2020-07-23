4420a8275c3ad1ab1a4832bfb4bf8bb7ab68fed8
Naveen Kumar B.N <navin205@gmail.com>
2020-07-23 12:30:54 +0530

    updated user data on c2 section

--- modules/ec2/instances.tf
+++ modules/ec2/instances.tf
@@ -5,6 +5,15 @@ resource "aws_instance" "phpapp" {
   associate_public_ip_address = "true"
   subnet_id = var.subnet_id
   availability_zone = "ap-south-1a"
+
+user_data = <<-EOF
+            #!/bin/bash
+            sudo apt update
+            sudo apt-get install -y apache2
+            sudo systemctl start apache2
+            sudo systemctl enable apache2
+            sudo bash -c 'echo I am your very first web server > /var/www/html/index.html'
+            EOF  
   
   tags = {
         Name = "phpapp"
